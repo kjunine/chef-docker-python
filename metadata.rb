2efb78734b5c6f8c5964d60bc92f038aa666fcdd
Daniel Ku <kjunine@gmail.com>
2014-11-20 15:11:22 +0900

    Adds initial version.

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'docker-python'
+maintainer       'Daniel Ku'
+maintainer_email 'kjunine@gmail.com'
+license          'MIT'
+description      'Installs/Configures docker-python'
+long_description 'Installs/Configures docker-python'
+version          '0.1.0'
+
+depends 'docker'
